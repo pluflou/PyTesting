a9d2ade22b15b7c552f20b211eedab15a57b1dc3
Dorota Jarecka <djarecka@gmail.com>
2019-05-01 22:12:05 -0400

    updating solutions

--- pycontest/utils.py
+++ pycontest/utils.py
@@ -1,13 +1,29 @@
 import numpy as np
 
 def E_kin(vel, mass):
-    """ calculate the kinematic energy of all particles """
+    """ calculate the kinematic energy of all particles.
+
+    Arguments:
+        vel: velocities
+        mass: masses
+
+    Returns:
+        Total kinetic energy
+    """
     vel = np.array(vel)
     mass = np.array(mass)
     return 0.5 * np.sum(mass * vel**2)
 
 def momentum(vel, mass):
-    """ calculate the momentum of all particles """
+    """ calculate the momentum of all particles
+
+    Arguments:
+        vel: velocities
+        mass: masses
+
+    Returns:
+            Total momentum
+    """
     vel = np.array(vel)
     mass = np.array(mass)
     return np.sum(mass * vel, axis=0)
